f2381e6908c7b2f834493d7d0eb8bec0eb9a1b1d
jaffaszt <jaffas@il.ibm.com>
2021-01-10 16:11:54 +0200

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,10 @@
-'''
-Created on 04-Sep-2019
+echo 'from flask import Flask
 
-@author: bkadambi
-'''
+app = Flask(__name__)
 
-# -*- coding: UTF-8 -*-
-"""
-hello_flask: First Python-Flask webapp
-"""
-from flask import Flask  # From module flask import class Flask
-app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+@app.route("/")
+def hello():
+    return "hello world!"
 
-@app.route('/')   # URL '/' to be handled by main() route handler
-def main():
-    """Say hello"""
-    return 'Hello, world!'
-
-if __name__ == '__main__':  # Script executed directly?
-    print("Hello World! Built with a Docker file.")
-    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)  # Launch built-in web server and run this Flask webapp
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")' > app.py
